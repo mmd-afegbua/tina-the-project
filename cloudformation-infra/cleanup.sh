aa85503eecdc5ab2899fdd72b3f32b02539c5735
mmd-afegbua <afegbuaismaila@gmail.com>
2020-04-27 00:09:19 +0100

    deleted tracked files

--- cloudformation-infra/cleanup.sh
+++
@@ -1,3 +0,0 @@
-echo "Cleaning up all stacks..."
-aws cloudformation delete-stack \
---stack-name $1 $2
--- cloudformation-infra/create.sh
+++
@@ -1,9 +0,0 @@
-echo "Creating stack..."
-
-aws cloudformation create-stack \
---stack-name $1 \
---template-body file://$2 \
---parameters file://$3 \
---region=us-west-2 \
---capabilities CAPABILITY_NAMED_IAM \
---disable-rollback
